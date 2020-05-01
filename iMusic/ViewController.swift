53819f31f8160cf5384e980468eb399af5e97509
Oleg <Sukhotskyi>
2020-05-02 01:46:13 +0300

    Project created

---
+++ iMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by Oleg Sukhotskiy on 02.05.2020.
+//  Copyright © 2020 Oleg Sukhotskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
